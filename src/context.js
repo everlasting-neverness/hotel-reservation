d7b47a2e6ce48fbd84fe50fefc372677f8dc92c3
Vasily <gerasimovv1990@gmail.com>
2020-02-29 14:01:37 +0300

    update components

---
+++ src/context.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import items from './data';
+
+const RoomContext = React.createContext();
+
+class RoomProvider extends Component {
+    state = {
+        rooms: [],
+        sortedRooms: [],
+        featuredRooms: [],
+        loading: true
+    };
+
+    componentDidMount() {
+        let rooms = this.formatData(items);
+        let featuredRooms = rooms.filter(room => room.featured === true);
+        this.setState({
+            rooms,
+            featuredRooms,
+            sortedRooms: rooms,
+            loading: false
+        });
+    }
+
+    formatData(items) {
+        return items.map(item => {
+            let id = item.sys.id,
+                images = item.fields.images.map(image => image.fields.file.url),
+                room = { ...item.fields, images, id };
+            return room;
+        });
+    }
+
+    render() {
+        return (
+            <RoomContext.Provider value={'hello'}>
+                {this.props.children}
+            </RoomContext.Provider>
+        );
+    }
+}
+
+const RoomConsumer = RoomContext.Consumer;
+
+export { RoomProvider, RoomConsumer, RoomContext };
