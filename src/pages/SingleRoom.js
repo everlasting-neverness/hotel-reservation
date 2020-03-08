e48e093d71826404a9f4f9d3bf76d86be2fabddc
Vasily <gerasimovv1990@gmail.com>
2020-03-08 08:50:05 +0300

    update

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Hero from '../components/Hero';
+import Banner from '../components/Banner';
+import { RoomContext } from '../context';
+import defaultBcg from '../images/room-1.jpeg';
+
 export default class SingleRoom extends Component {
     render() {
         return (
