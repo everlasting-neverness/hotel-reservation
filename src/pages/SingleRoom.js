9a75e1204ea78c810ea583a93d2a3a4f806a5c35
Vasily <gerasimovv1990@gmail.com>
2020-03-12 08:42:30 +0300

    update

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -6,9 +6,50 @@ import { RoomContext } from '../context';
 import defaultBcg from '../images/room-1.jpeg';
 
 export default class SingleRoom extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            slug: this.props.match.params.slug,
+            defaultBcg
+        };
+    }
+
+    static contextType = RoomContext;
+
     render() {
+        const { getRoom } = this.context,
+            room = getRoom(this.state.slug);
+
+        if (!room) {
+            return (
+                <div className='error'>
+                    <h3>No such room could be found...</h3>
+                    <Link to='/rooms' className='btn-primary'>
+                        Back to rooms
+                    </Link>
+                </div>
+            );
+        }
+
+        const {
+            name,
+            description,
+            images,
+            capacity,
+            size,
+            price,
+            extras,
+            brekfast,
+            pets
+        } = room;
+
         return (
             <Hero hero='roomsHero'>
+                <Banner title={`${name} room`}>
+                    <Link to='/rooms' className='btn-primary'>
+                        Back to rooms
+                    </Link>
+                </Banner>
                 <div>SingleRoom</div>
             </Hero>
         );
