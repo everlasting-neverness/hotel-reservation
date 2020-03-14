ae8392a5cdc5910486f33ffb2897154fd5ba8204
Vasily <gerasimovv1990@gmail.com>
2020-03-14 08:44:24 +0300

    update roomslist layout

--- src/components/RoomsList.js
+++ src/components/RoomsList.js
@@ -2,16 +2,21 @@ import React from 'react';
 import Room from './Room';
 
 export default function RoomsList({ rooms }) {
-
-    if (!rooms) {
-        return null;
+    if (!rooms.length) {
+        return (
+            <div className='empty-search'>
+                <h3>unfortunately no rooms matched your search parameters</h3>
+            </div>
+        );
     }
 
     return (
-        <div className='roomslist-center'>
-            {rooms.map(room => (
-                <Room key={room.id} room={room} />
-            ))}
-        </div>
-    )
+        <section className='roomslist'>
+            <div className='roomslist-center'>
+                {rooms.map(item => {
+                    return <Room key={item.id} room={item} />;
+                })}
+            </div>
+        </section>
+    );
 }
